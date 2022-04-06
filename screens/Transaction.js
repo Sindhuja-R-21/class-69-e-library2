aafedd1d2256fe3f1c874e3506c8262d38aded95
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-04-06 20:38:20 +0530

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class TransactionScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Transaction Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5653D4',
+  },
+  text: {
+    color: '#ffff',
+    fontSize: 15,
+  },
+  button: {
+    width: '43%',
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F48D20',
+    borderRadius: 15,
+  },
+  buttonText: {
+    fontSize: 24,
+    color: '#FFFFFF',
+  },
+});
